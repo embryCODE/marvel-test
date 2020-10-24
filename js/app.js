e84021bb49d1f41f3268437cb6a22d421419af42
Mason Embry <mason@embrycode.com>
2020-10-24 09:29:54 -0500

    Build out app

--- js/app.js
+++ js/app.js
@@ -4,4 +4,73 @@ const COMICS_URL = BASE_URL + 'comics?apikey=' + API_KEY
 
 fetch(COMICS_URL)
   .then((res) => res.json())
-  .then((res) => console.log(res.data))
+  .then((res) => handleComicsData(res.data))
+
+function handleComicsData(comicsData) {
+  const appDiv = document.getElementById('app')
+  const formattedComicsData = formatComicsData(comicsData)
+
+  buildApp(appDiv, formattedComicsData)
+}
+
+function formatComicsData(comicsData) {
+  const formattedComicsData = []
+
+  for (let i = 0; i < comicsData.results.length; i++) {
+    const thisResult = comicsData.results[i]
+
+    formattedComicsData.push({
+      id: thisResult.id,
+      title: thisResult.title,
+      description: thisResult.description,
+    })
+  }
+
+  return formattedComicsData
+}
+
+function buildApp(appDiv, data) {
+  const characterDivs = buildCharacterDivs(data)
+
+  characterDivs.forEach((characterDiv) => {
+    appDiv.appendChild(characterDiv)
+  })
+}
+
+function buildCharacterDivs(characters) {
+  const characterDivs = characters.map((character) => {
+    return buildCharacterDiv(character)
+  })
+
+  return characterDivs
+}
+
+function buildCharacterDiv(character) {
+  const characterHeading = buildCharacterHeading(character.title)
+  const characterParagraph = buildCharacterParagraph(character.description)
+  const characterDiv = fillCharacterDiv(characterHeading, characterParagraph)
+
+  return characterDiv
+}
+
+function buildCharacterHeading(characterTitle) {
+  const h3El = document.createElement('h3')
+  h3El.innerText = characterTitle
+
+  return h3El
+}
+
+function buildCharacterParagraph(characterDescription) {
+  const pEl = document.createElement('p')
+  pEl.innerHTML = characterDescription
+
+  return pEl
+}
+
+function fillCharacterDiv(characterHeading, characterParagraph) {
+  const filledDiv = document.createElement('div')
+  filledDiv.appendChild(characterHeading)
+  filledDiv.appendChild(characterParagraph)
+
+  return filledDiv
+}
